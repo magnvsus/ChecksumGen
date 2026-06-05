08d8de9e84ce617462c4898ade611d3efa9b89f5
magnvsus <magnvsus@gmail.com>
2026-06-05 15:18:04 -0400

    feat: word building using file input
    pa01.c
    - Created word building functionality.
      o char is read from file, then bitwise pushed to left of word buffer
    
    TODO
    - Create list of words.
    - Add word adding functionality.
    - Add two's complement reading.

--- pa01.c
+++ pa01.c
@@ -1,3 +1,6 @@
+// Compile with gcc -lm pa01.c -o pa01
+// Run with ./pa01 <filename> <checksum>
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -21,6 +24,52 @@ int main(int argc, char *argv[])
         fprintf(stderr, "Invalid file name.");
         return 1;
     }
+
+
+    //-- Get size of file
+    // Send cursor to end, then ftell
+    fseek(file, 0, SEEK_END);
+    unsigned int fileSize = (unsigned int)ftell(file) - 1;
+    printf("fS:%u\n", fileSize);
+
+    // Return cursor to start
+    fseek(file, 0, SEEK_SET);
+    
+    //-- Calculate padding requirement
+    // Calculate num of characters loaded into checksum word.
+    unsigned int charCount = checksumSize/8;
+    printf("cC:%u\n", charCount);
+    // Caclulate padding. 
+    unsigned int remainder = fileSize % charCount;
+    unsigned int paddingCount = (remainder) ? charCount - remainder : 0;
+    printf("pC:%u\n", paddingCount);
+    //-- Move chars into checksum 
+    unsigned int checksumListSize = (unsigned int)(ceil((double)fileSize/charCount));
+    printf("lS:%d\n", checksumListSize);
+    unsigned int checksumList[checksumListSize];
+    unsigned int checksum = 0;
+    int loopCount, inCount = fileSize;
+
+    
+    // Loop for each word
+    for(int i = 0; i < checksumListSize; i++) {
+        
+        // Loop for each char
+        for(loopCount = charCount; loopCount > 0; loopCount--)
+        {
+            if(inCount > 0) {
+                checksum = checksum | ((unsigned char)fgetc(file) << (loopCount-1)*8);
+                inCount--;
+                printf("%d-%.*b\n",loopCount, checksumSize, checksum);
+            }
+            else {
+                checksum = checksum | (unsigned char)'X' << (loopCount-1) * 8;
+                printf("%d-%.*b\n",loopCount, checksumSize, checksum);
+            }
+        }
+    }
+        
+
 
     fclose(file);
     return 0;
